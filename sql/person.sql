7effba41794ed6c8245cdf8b1e3317b0cb3562ba
eyeooo <edison.lando@gmail.com>
2014-09-12 23:43:43 +0800

    add house.sql,person.sql

---
+++ sql/person.sql
@@ -0,0 +1,27 @@
+/*
+SQLyog Enterprise - MySQL GUI
+Host - 5.5.23 
+*********************************************************************
+Server version : 5.5.23
+*/
+/*!40101 SET NAMES utf8 */;
+
+create table `person` (
+	`id` double ,
+	`user_id` double ,
+	`name` varchar (765),
+	`id_type` varchar (765),
+	`id_number` varchar (765),
+	`regions` varchar (765),
+	`marriage_status` double ,
+	`children` double ,
+	`education` double ,
+	`salary` double ,
+	`insurance` double ,
+	`insurance_num` double ,
+	`house_condition` double ,
+	`is_buy_car` double ,
+	`is_overdue` double ,
+	`created_on` datetime ,
+	`modified_on` datetime 
+); 
