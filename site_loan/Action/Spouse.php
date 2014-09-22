681107367e052ae3d36a270e4c00a30e923d6c77
tim1860 <408016695@qq.com>
2014-09-22 23:04:35 +0800

    添加一些action

---
+++ site_loan/Action/Spouse.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: wanwenyou
+ * Date: 13-9-13
+ * Time: 上午11:14
+ * To change this template use File | Settings | File Templates.
+ */
+
+namespace App\Action;
+
+
+class Spouse extends \Lavender\WebPage
+{
+
+	protected $without_auth_actions = array(
+		'*',
+	);
+
+	public function  index_action()
+	{
+
+	}
+
+}
